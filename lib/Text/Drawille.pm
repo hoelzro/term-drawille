8d7283a68663b5162248cb4eeb7fdf3520ead04f
Rob Hoelz <rob@hoelz.ro>
2014-05-23 23:35:45 -0500

    initial commit

---
+++ lib/Text/Drawille.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Text::Drawille;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT: A short description of Text::Drawille
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
